a2cb31f06c2eec0d163b6c67658a54343fa072bd
Gabriel Badarau <gabriel.badarau@endava.com>
2023-11-16 16:28:09 +0200

    wider time zone

--- actions/get-vacations.ts
+++ actions/get-vacations.ts
@@ -17,8 +17,8 @@ export const getVacations = async (
     const currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
     // I have to decrease time because of the conflict timezones between server and client, and because I don't send any data from client to server in order for him to know in what time zone the request was made so that it could give me actual start of the day
-    // timezone GMT +2, so this is temporary fix
-    const decreasedDate = new Date(currentDate.getTime() - 2 * 60 * 60 * 1000);
+    // timezone GMT +2 + added a few hours extra in case, so this is temporary fix
+    const decreasedDate = new Date(currentDate.getTime() - 5 * 60 * 60 * 1000);
 
     let usersVacations = [];
     let skip = 0;
